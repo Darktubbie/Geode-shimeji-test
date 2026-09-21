aa4b3f7bad1abd3f546c15d3a6fff7dc5bebdb8b
Darktubbie <58957655+Darktubbie@users.noreply.github.com>
2026-09-21 14:42:37 -0600

    Create ShimejiAction.hpp

---
+++ src/shimeji/ShimejiAction.hpp
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <Geode/Geode.hpp>
+#include <string>
+#include <vector>
+
+struct ShimejiPose {
+    std::string image;
+    cocos2d::CCPoint imageAnchor;
+    cocos2d::CCPoint velocity;
+    float duration = 0.f;
+};
+
+struct ShimejiAnimation {
+    std::vector<ShimejiPose> poses;
+};
+
+struct ShimejiAction {
+    std::string name;
+    std::string type;
+    std::string borderType;
+    ShimejiAnimation animation;
+};
