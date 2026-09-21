31dd1e19415669018543182ac5529672a92c2e1a
Darktubbie <58957655+Darktubbie@users.noreply.github.com>
2026-09-21 11:35:49 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,15 +2,59 @@
 
 using namespace geode::prelude;
 
-namespace {
-    // Prueba mínima absoluta: solo confirma que la llamada ocurre.
-    // Sin filesystem, sin dlopen, sin Discord, sin nada más.
-    void runDiscordSdkSmokeTest() {
-        log::info("[DiscordSDKTest] FUNCTION ENTERED");
+class ShimejiLayer : public CCLayer {
+public:
+    static ShimejiLayer* create() {
+        auto ret = new ShimejiLayer();
+
+        if (ret && ret->init()) {
+            ret->autorelease();
+            return ret;
+        }
+
+        delete ret;
+        return nullptr;
     }
-}
+
+    bool init() {
+        if (!CCLayer::init()) {
+            return false;
+        }
+
+        auto sprite = CCSprite::create("shimeji/test.png");
+
+        if (!sprite) {
+            log::error("Failed to load shimeji/test.png");
+            return true;
+        }
+
+        sprite->setPosition({100.f, 100.f});
+        this->addChild(sprite);
+
+        log::info("Test Shimeji loaded successfully!");
+
+        return true;
+    }
+};
 
 $on_mod(Loaded) {
-    log::info("Geode RPC Test loaded successfully!");
-    runDiscordSdkSmokeTest();
+    log::info("Geode Shimeji Test loaded!");
+
+    auto scene = CCDirector::sharedDirector()->getRunningScene();
+
+    if (!scene) {
+        log::error("No running scene found!");
+        return;
+    }
+
+    auto layer = ShimejiLayer::create();
+
+    if (!layer) {
+        log::error("Failed to create ShimejiLayer!");
+        return;
+    }
+
+    scene->addChild(layer, 9999);
+
+    log::info("Shimeji layer added to current scene!");
 }
